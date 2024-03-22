17f062c98bf9a646a662d9288d7b78f187885f0c
Sanhita <sacred.profiles.02@gmail.com>
2024-03-22 20:20:13 +0530

    form ui done

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', function() {
-        alert('Button clicked!');
-    });
-});
-
-
